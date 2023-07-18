57e77e4ec794b4e1a5c3737bf7e8e07826cf4f9a
Denis-Semenikhin <denis-semenikhin@yandex.ru>
2023-07-18 16:43:34 +0300

    2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Переменные
 const word0 = "dog";
 const world0Translation = "собака";
 
@@ -16,25 +17,64 @@ let correctAnswersCount = 0;
 
 const correctAnswersMinPercent = 50;
 
-//---------
+//Слово 0
 
-const userAnswer0 = prompt(word0); //0
-alert(userAnswer0 === world0Translation);
+const userAnswer0 = prompt(word0);
+
 if (userAnswer0 === world0Translation) {
   correctAnswersCount = correctAnswersCount + 1;
 }
+if (userAnswer0 === world0Translation) {
+  alert("Правильно");
+} else {
+  alert("Не правильно");
+}
+document.write("Твой ответ №1:" + "<br>" + userAnswer0 + "<br>");
+if (userAnswer0 === world0Translation) {
+  document.write("Правильно" + "<br>");
+} else {
+  document.write("Не правильно" + "<br>");
+}
 
-const userAnswer1 = prompt(word1); //1
-alert(userAnswer1 === world1Translation);
+//Слово 1
+
+const userAnswer1 = prompt(word1);
+
 if (userAnswer1 === world1Translation) {
   correctAnswersCount = correctAnswersCount + 1;
 }
+if (userAnswer1 === world1Translation) {
+  alert("Правильно");
+} else {
+  alert("Не правильно");
+}
+document.write("Твой ответ №2:" + "<br>" + userAnswer1 + "<br>");
+if (userAnswer1 === world1Translation) {
+  document.write("Правильно" + "<br>");
+} else {
+  document.write("Не правильно" + "<br>");
+}
 
-const userAnswer2 = prompt(word2); //2
-alert(userAnswer2 === world2Translation);
+//Слово 2
+
+const userAnswer2 = prompt(word2);
+
 if (userAnswer2 === world2Translation) {
   correctAnswersCount = correctAnswersCount + 1;
 }
+if (userAnswer2 === world2Translation) {
+  alert("Правильно");
+} else {
+  alert("Не правильно");
+}
+document.write("Твой ответ №3:" + "<br>" + userAnswer2 + "<br>");
+if (userAnswer2 === world2Translation) {
+  document.write("Правильно" + "<br>");
+} else {
+  document.write("Не правильно" + "<br>");
+}
+
+//Результат
 
 const userCorrectAnswersPecent = (correctAnswersCount / wordsCount) * 100;
 
