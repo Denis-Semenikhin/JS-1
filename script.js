309aa2096350eb823c422ac6936dc25133fd0784
Denis-Semenikhin <denis-semenikhin@yandex.ru>
2023-08-17 17:06:10 +0300

    1

--- script.js
+++ script.js
@@ -1,36 +1,55 @@
 //Переменные
-const word0 = "dog";
-const world0Translation = "собака";
 
-const word1 = "cat";
-const world1Translation = "кошка";
+const slovar = {
+  words: {
+    word0: {
+      original: "dog",
+      translation: "собака",
+    },
+    word1: {
+      original: "cat",
+      translation: "кошка",
+    },
+    word2: {
+      original: "fish",
+      translation: "рыба",
+    },
+  },
+  wordsCount: 3,
+};
 
-const word2 = "fish";
-const world2Translation = "рыба";
+const messages = {
+  start: {
+    hello: "Здорово чувак!",
+  },
+  result: {
+    finishSuccess: "Молодец",
+    finishUnSuccess: "Ты дебил",
+  },
+};
 
-const wordsCount = 3;
+const settings = {
+  correctAnswersMinPercent: 50,
+};
+const result = {
+  correctAnswersCount: 0,
+};
 
-const finishSuccessMessage = "Молодец";
-const finishUnSuccessMessage = "Ты дебил";
-
-let correctAnswersCount = 0;
-
-const correctAnswersMinPercent = 50;
-
+alert(messages.start.hello);
 //Слово 0
 
-const userAnswer0 = prompt(word0);
+const userAnswer0 = prompt(slovar.words.word0.original);
 
-if (userAnswer0 === world0Translation) {
-  correctAnswersCount = correctAnswersCount + 1;
+if (userAnswer0 === slovar.words.word0.translation) {
+  result.correctAnswersCount = result.correctAnswersCount + 1;
 }
-if (userAnswer0 === world0Translation) {
+if (userAnswer0 === slovar.words.word0.translation) {
   alert("Правильно");
 } else {
   alert("Не правильно");
 }
 document.write("Твой ответ №1:" + "<br>" + userAnswer0 + "<br>");
-if (userAnswer0 === world0Translation) {
+if (userAnswer0 === slovar.words.word0.translation) {
   document.write("Правильно" + "<br>");
 } else {
   document.write("Не правильно" + "<br>");
@@ -38,18 +57,18 @@ if (userAnswer0 === world0Translation) {
 
 //Слово 1
 
-const userAnswer1 = prompt(word1);
+const userAnswer1 = prompt(slovar.words.word1.original);
 
-if (userAnswer1 === world1Translation) {
-  correctAnswersCount = correctAnswersCount + 1;
+if (userAnswer1 === slovar.words.word1.translation) {
+  result.correctAnswersCount = result.correctAnswersCount + 1;
 }
-if (userAnswer1 === world1Translation) {
+if (userAnswer1 === slovar.words.word1.translation) {
   alert("Правильно");
 } else {
   alert("Не правильно");
 }
 document.write("Твой ответ №2:" + "<br>" + userAnswer1 + "<br>");
-if (userAnswer1 === world1Translation) {
+if (userAnswer1 === slovar.words.word1.translation) {
   document.write("Правильно" + "<br>");
 } else {
   document.write("Не правильно" + "<br>");
@@ -57,18 +76,18 @@ if (userAnswer1 === world1Translation) {
 
 //Слово 2
 
-const userAnswer2 = prompt(word2);
+const userAnswer2 = prompt(slovar.words.word2.original);
 
-if (userAnswer2 === world2Translation) {
-  correctAnswersCount = correctAnswersCount + 1;
+if (userAnswer2 === slovar.words.word2.translation) {
+  result.correctAnswersCount = result.correctAnswersCount + 1;
 }
-if (userAnswer2 === world2Translation) {
+if (userAnswer2 === slovar.words.word2.translation) {
   alert("Правильно");
 } else {
   alert("Не правильно");
 }
 document.write("Твой ответ №3:" + "<br>" + userAnswer2 + "<br>");
-if (userAnswer2 === world2Translation) {
+if (userAnswer2 === slovar.words.word2.translation) {
   document.write("Правильно" + "<br>");
 } else {
   document.write("Не правильно" + "<br>");
@@ -76,10 +95,11 @@ if (userAnswer2 === world2Translation) {
 
 //Результат
 
-const userCorrectAnswersPecent = (correctAnswersCount / wordsCount) * 100;
+const userCorrectAnswersPecent =
+  (result.correctAnswersCount / slovar.wordsCount) * 100;
 
-if (userCorrectAnswersPecent > correctAnswersMinPercent) {
-  alert(finishSuccessMessage);
+if (userCorrectAnswersPecent > settings.correctAnswersMinPercent) {
+  alert(messages.result.finishSuccess);
 } else {
-  alert(finishUnSuccessMessage);
+  alert(messages.result.finishUnSuccess);
 }
